27281906706ba76372592ec6c5da992360a5948a
kenro-t <ngkn1425@gmail.com>
2025-03-08 17:28:53 +0900

    ハンドラ重複の修正

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,7 +6,7 @@ import icon from '../../resources/icon.png?asset'
 import { getAccounts, registerAccount, unregisterAccount, Account } from './ipc-handlers/totp'
 import { createWatchedAccounts } from './utils'
 
-function createWindow(): BrowserWindow {
+function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 400,
@@ -34,33 +34,6 @@ function createWindow(): BrowserWindow {
     shell.openExternal(details.url)
     return { action: 'deny' }
   })
-
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
-  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
-  } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
-  }
-
-  return mainWindow
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
-
-  const mainWindow = createWindow()
 
   // DOM が読み込まれたらアカウント情報を取得して監視する
   let accounts: Account[] = []
@@ -123,6 +96,37 @@ app.whenReady().then(() => {
   ipcMain.handle('/show-alert', (_, ...messages) => {
     dialog.showErrorBox('error', messages.join('\n'))
   })
+
+  // HMR for renderer base on electron-vite cli.
+  // Load the remote URL for development or the local html file for production.
+  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  } else {
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+
+  mainWindow.on('close', () => {
+    ipcMain.removeHandler('/account/register')
+    ipcMain.removeHandler('/account/unregister')
+    ipcMain.removeHandler('/show-alert')
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  // Set app user model id for windows
+  electronApp.setAppUserModelId('com.electron')
+
+  // Default open or close DevTools by F12 in development
+  // and ignore CommandOrControl + R in production.
+  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  app.on('browser-window-created', (_, window) => {
+    optimizer.watchWindowShortcuts(window)
+  })
+
+  createWindow()
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
