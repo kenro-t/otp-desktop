a2845ad1a1ec086cec6f2e26d3971105e3d1a7f2
kenro-t <ngkn1425@gmail.com>
2025-03-08 16:35:01 +0900

    ウィンドウ生成時にハンドラが重複する問題の調整

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,7 +6,7 @@ import icon from '../../resources/icon.png?asset'
 import { getAccounts, registerAccount, unregisterAccount, Account } from './ipc-handlers/totp'
 import { createWatchedAccounts } from './utils'
 
-function createWindow(): void {
+function createWindow(): BrowserWindow {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 400,
@@ -42,6 +42,25 @@ function createWindow(): void {
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
+
+  return mainWindow
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  // Set app user model id for windows
+  electronApp.setAppUserModelId('com.electron')
+
+  // Default open or close DevTools by F12 in development
+  // and ignore CommandOrControl + R in production.
+  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  app.on('browser-window-created', (_, window) => {
+    optimizer.watchWindowShortcuts(window)
+  })
+
+  const mainWindow = createWindow()
 
   // DOM が読み込まれたらアカウント情報を取得して監視する
   let accounts: Account[] = []
@@ -104,26 +123,6 @@ function createWindow(): void {
   ipcMain.handle('/show-alert', (_, ...messages) => {
     dialog.showErrorBox('error', messages.join('\n'))
   })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
-
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
-
-  createWindow()
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
